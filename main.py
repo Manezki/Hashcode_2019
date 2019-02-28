8e917e822e8acbc33cd61e7177a0d8dbafe6437e
Janne Holopainen <janne.holopainen@reaktor.com>
2019-02-28 20:29:15 +0200

    WIP MVP

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
 from hash_io import read_file
 from os import path as op
+import random
 
-print(read_file(op.join(op.dirname(__file__), "data", "a_example.txt")))
+def metric(left, right):
+    inter = left & right
+    l_out = left - right
+    r_out = right - left
+    return min(inter, l_out, r_out)
+
+if __name__ == "__main__":
+    example_a = read_file(op.join(op.dirname(__file__), "data", "a_example.txt"))
+    
+    photos = [k for k, _ in example_a.items()]
+
+    out = [random.choice(photos)]
+
+    
+
+
+    
